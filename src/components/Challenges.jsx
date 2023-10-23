81575ed2cc920824eb55e14b2845fc3b77d945d3
Emad Battal <emadbattal.work@gmail.com>
2023-10-23 18:43:28 +0300

    finished section 26

--- src/components/Challenges.jsx
+++ src/components/Challenges.jsx
@@ -1,12 +1,14 @@
-import { useContext, useState } from 'react';
+import { useContext, useState } from "react";
 
-import { ChallengesContext } from '../store/challenges-context.jsx';
-import ChallengeItem from './ChallengeItem.jsx';
-import ChallengeTabs from './ChallengeTabs.jsx';
+import { AnimatePresence, motion } from "framer-motion";
+
+import { ChallengesContext } from "../store/challenges-context.jsx";
+import ChallengeItem from "./ChallengeItem.jsx";
+import ChallengeTabs from "./ChallengeTabs.jsx";
 
 export default function Challenges() {
   const { challenges } = useContext(ChallengesContext);
-  const [selectedType, setSelectedType] = useState('active');
+  const [selectedType, setSelectedType] = useState("active");
   const [expanded, setExpanded] = useState(null);
 
   function handleSelectType(newType) {
@@ -24,11 +26,11 @@ export default function Challenges() {
   }
 
   const filteredChallenges = {
-    active: challenges.filter((challenge) => challenge.status === 'active'),
+    active: challenges.filter((challenge) => challenge.status === "active"),
     completed: challenges.filter(
-      (challenge) => challenge.status === 'completed'
+      (challenge) => challenge.status === "completed"
     ),
-    failed: challenges.filter((challenge) => challenge.status === 'failed'),
+    failed: challenges.filter((challenge) => challenge.status === "failed"),
   };
 
   const displayedChallenges = filteredChallenges[selectedType];
@@ -40,19 +42,40 @@ export default function Challenges() {
         onSelectType={handleSelectType}
         selectedType={selectedType}
       >
-        {displayedChallenges.length > 0 && (
-          <ol className="challenge-items">
-            {displayedChallenges.map((challenge) => (
-              <ChallengeItem
-                key={challenge.id}
-                challenge={challenge}
-                onViewDetails={() => handleViewDetails(challenge.id)}
-                isExpanded={expanded === challenge.id}
-              />
-            ))}
-          </ol>
-        )}
-        {displayedChallenges.length === 0 && <p>No challenges found.</p>}
+        {/* we have to set the mode to wait to make sure that motion waits for an animation to finishes before starting another one, for example to fix the issue of the fallback text displaying right when the list is empty */}
+        <AnimatePresence mode="wait">
+          {/* whenever we have more than one component between AnimatePresence we have to add the key prop to help motion tell them apart */}
+          {displayedChallenges.length > 0 && (
+            <motion.ol
+              key="list"
+              className="challenge-items"
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ x: 50, opacity: 0 }}
+            >
+              <AnimatePresence>
+                {displayedChallenges.map((challenge) => (
+                  <ChallengeItem
+                    key={challenge.id}
+                    challenge={challenge}
+                    onViewDetails={() => handleViewDetails(challenge.id)}
+                    isExpanded={expanded === challenge.id}
+                  />
+                ))}
+              </AnimatePresence>
+            </motion.ol>
+          )}
+          {displayedChallenges.length === 0 && (
+            <motion.p
+              initial={{ opacity: 0, x: 50 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 50 }}
+              key="fallback"
+            >
+              No challenges found.
+            </motion.p>
+          )}
+        </AnimatePresence>
       </ChallengeTabs>
     </div>
   );
--- src/components/NewChallenge.jsx
+++ src/components/NewChallenge.jsx
@@ -1,5 +1,5 @@
 import { useContext, useRef, useState } from "react";
-import { motion } from "framer-motion";
+import { motion, useAnimate, stagger } from "framer-motion";
 
 import { ChallengesContext } from "../store/challenges-context.jsx";
 import Modal from "./Modal.jsx";
@@ -9,6 +9,10 @@ export default function NewChallenge({ onDone }) {
   const title = useRef();
   const description = useRef();
   const deadline = useRef();
+
+  //scope is a ref we can add to elements, animate is a function which we can use in the code to imperatively trigger an animation'
+  //we add the scope ref to the form tag because we want to specify where to look for inputs and textareas to animate. we dont want to select from the whole page
+  const [scope, animate] = useAnimate();
 
   const [selectedImage, setSelectedImage] = useState(null);
   const { addChallenge } = useContext(ChallengesContext);
@@ -32,6 +36,13 @@ export default function NewChallenge({ onDone }) {
       !challenge.deadline.trim() ||
       !challenge.image
     ) {
+      animate(
+        "input, textarea",
+        { x: [-5, -10, -5, 0, 5, 10, 5, 0] },
+        { type: "spring", duration: 0.5, delay: stagger(0.05) }
+      );
+
+      animate("label", { color: "red" }, { type: "spring", duration: 0.5 });
       return;
     }
 
@@ -41,15 +52,26 @@ export default function NewChallenge({ onDone }) {
 
   return (
     <Modal title="New Challenge" onClose={onDone}>
-      <form id="new-challenge" onSubmit={handleSubmit}>
+      <form id="new-challenge" onSubmit={handleSubmit} ref={scope}>
         <p>
           <label htmlFor="title">Title</label>
-          <input ref={title} type="text" name="title" id="title" />
+          <input
+            ref={title}
+            type="text"
+            name="title"
+            id="title"
+            placeholder="title"
+          />
         </p>
 
         <p>
           <label htmlFor="description">Description</label>
-          <textarea ref={description} name="description" id="description" />
+          <textarea
+            ref={description}
+            name="description"
+            id="description"
+            placeholder="describe your challenge"
+          />
         </p>
 
         <p>
@@ -57,12 +79,19 @@ export default function NewChallenge({ onDone }) {
           <input ref={deadline} type="date" name="deadline" id="deadline" />
         </p>
 
-        <ul id="new-challenge-images">
+        {/* to stagger list element we have to go to the parent of li and use transition with staggerChildren set to our desired delay */}
+        <motion.ul
+          id="new-challenge-images"
+          variants={{
+            visible: { transition: { staggerChildren: 0.05 } },
+          }}
+        >
           {images.map((image) => (
             <motion.li
               variants={{
                 hidden: { scale: 0.5, opacity: 0 },
-                visible: { scale: 1, opacity: 1 },
+                // we can set keyframes as values
+                visible: { scale: 1, opacity: [0.8, 1.3, 1] },
               }}
               // exit={{scale: 1, opacity: 1}}
               transition={{
@@ -75,7 +104,7 @@ export default function NewChallenge({ onDone }) {
               <img {...image} />
             </motion.li>
           ))}
-        </ul>
+        </motion.ul>
 
         <p className="new-challenge-actions">
           <button type="button" onClick={onDone}>
